c4c3c4a65779d73d84b21d262af16ce7e9f3d203
Flaxo P <jack.f.pas@gmail.com>
2024-05-17 21:01:50 -0400

    simplicity trial 6 re-name file name

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    hcp = {
+      source  = "hashicorp/hcp"
+      version = "~> 0.27.0" # Adjust the version as necessary
+    }
+  }
+}
+
+provider "hcp" {
+  // HCP provider configuration if needed
+}
+
+resource "hcp_hvn" "learn_hcp_vault_hvn" {
+  hvn_id         = var.hvn_id
+  cloud_provider = var.cloud_provider
+  region         = var.region
+}
+
+resource "hcp_vault_cluster" "learn_hcp_vault" {
+  hvn_id     = hcp_hvn.learn_hcp_vault_hvn.hvn_id
+  cluster_id = var.cluster_id
+  tier       = var.tier
+  # public_endpoint = true
+}
