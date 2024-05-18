5f8c785e334001c0c0f5716ecf747d6bba749d64
Flaxo P <jack.f.pas@gmail.com>
2024-05-17 20:43:54 -0400

    reducing file size, increase simplicty

--- vault.tf
+++ vault.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    hcp = {
+      source  = "hashicorp/hcp"
+      version = "~> 0.27.0" # Adjust the version as necessary
+    }
+  }
+}
+
+provider "hcp" {
+  // Configure the HCP provider if necessary
+}
+
 resource "hcp_hvn" "learn_hcp_vault_hvn" {
   hvn_id         = var.hvn_id
   cloud_provider = var.cloud_provider
