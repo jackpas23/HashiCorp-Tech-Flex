2980ace15c4eb2e083e38b183b45982a8669f9a2
Flaxo P <jack.f.pas@gmail.com>
2024-05-18 01:13:32 -0400

    day 1 complete, running script and module

--- vault_setup/main.tf
+++ vault_setup/main.tf
@@ -7,7 +7,7 @@ module "vault_onboarding" {
   source        = "../vault-onboarding"
   vault_address = var.vault_address
   vault_token   = var.vault_token
-  namespace     = "example-namespace"
+  namespace     = "example-snamespace"
   username      = "example-user"
   password      = "example-password"
 }
@@ -15,11 +15,11 @@ module "vault_onboarding" {
 variable "vault_address" {
   description = "The address of the Vault server"
   type        = string
-  default     = "https://vault.example.com"
+  #default     = "https://vault.example.com"
 }
 
 variable "vault_token" {
   description = "The token to authenticate with Vault"
   type        = string
-  default     = ""  # Optional default value
+  #default     = ""  # Optional default value
 }
