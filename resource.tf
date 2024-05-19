9a6ba3561565cad4b8eeebf971b26a8e3123ab90
Flaxo P <jack.f.pas@gmail.com>
2024-05-19 16:31:42 -0400

    modified aws module so id and secret key aren't plaintexted

--- resource.tf
+++ resource.tf
@@ -29,8 +29,8 @@ module "aws_secrets_engine" {
   vault_address = hcp_vault_cluster.hcp_vault_cluster1.vault_public_endpoint_url
   vault_token   = hcp_vault_cluster_admin_token.vault_admin.token
   aws_backend_path = "aws"
-  aws_access_key   = "AKIA....."
-  aws_secret_key   = "AWS secret key"
+  aws_access_key   = var.aws_access_key
+  aws_secret_key   = var.aws_secret_key
   aws_region       = "us-east-1"
   role_name        = "my-role"
   policy_name      = "team-policy"
