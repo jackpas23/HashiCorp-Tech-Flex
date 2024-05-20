5d233b576f554fabca0cf93884b69640842ff7d3
Flaxo P <jack.f.pas@gmail.com>
2024-05-20 12:45:44 -0400

    fixed with push

--- main.tf
+++ main.tf
@@ -1,23 +1,23 @@
 
-resource "hcp_hvn" "hcp_vault_hvn1" {
+resource "hcp_hvn" "hcp_vault_hvn" {
   hvn_id         = var.hvn_id
   cloud_provider = var.cloud_provider
   region         = var.aws_region
 }
 resource "hcp_vault_cluster" "hcp_vault_cluster" {
-  hvn_id          = hcp_hvn.hcp_vault_hvn1.hvn_id
+  hvn_id          = hcp_hvn.hcp_vault_hvn.hvn_id
   cluster_id      = var.cluster_id
   tier            = var.tier
   public_endpoint = true
 }
 resource "hcp_vault_cluster_admin_token" "vault_admin" {
-  cluster_id = hcp_vault_cluster.hcp_vault_cluster1.cluster_id
-  depends_on = [hcp_vault_cluster.hcp_vault_cluster1]
+  cluster_id = hcp_vault_cluster.hcp_vault_cluster.cluster_id
+  depends_on = [hcp_vault_cluster.hcp_vault_cluster]
 }
 
 module "vault_onboarding" {
   source        = "./modules/vault-onboarding"
-  vault_address = hcp_vault_cluster.hcp_vault_cluster1.vault_public_endpoint_url
+  vault_address = hcp_vault_cluster.hcp_vault_cluster.vault_public_endpoint_url
   vault_token   = hcp_vault_cluster_admin_token.vault_admin.token
   namespace     = var.namespace
   username      = var.username
@@ -27,7 +27,7 @@ module "vault_onboarding" {
 
 module "aws_secrets_engine" {
   source  = "./modules/aws-secrets"
-  vault_address = hcp_vault_cluster.hcp_vault_cluster1.vault_public_endpoint_url
+  vault_address = hcp_vault_cluster.hcp_vault_cluster.vault_public_endpoint_url
   vault_token   = hcp_vault_cluster_admin_token.vault_admin.token
   aws_backend_path = var.aws_backend_path
   aws_access_key   = var.aws_access_key
