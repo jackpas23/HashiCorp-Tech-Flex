1ff32a8b578bb26c40b7d8b53ca7c7a9a4869f12
Flaxo P <jack.f.pas@gmail.com>
2024-05-20 12:25:36 -0400

    modified setup.sh

--- setup.sh
+++ setup.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
 # Prompt the user for input
-read -p "Enter the Vault address (vault_address): " vault_address
-read -p "Enter the Vault admin token (vault_admin_token): " vault_admin_token
-read -p "Enter the namespace (namespace): " namespace
+read -p "Enter the namespace (namespace) [default: dev]: " namespace
 read -p "Enter the AWS Access Key ID (aws_access_key): " aws_access_key
 read -sp "Enter the AWS Secret Key (aws_secret_key): " aws_secret_key
 echo
 read -p "Enter the AWS region (aws_region) [default: us-east-1]: " aws_region
 aws_region=${aws_region:-us-east-1}
-read -p "Enter the role name (role_name): " role_name
-read -p "Enter the user policy name (user_policy_name): " user_policy_name
-read -p "Enter the application policy name (app_policy_name): " app_policy_name
-read -p "Enter the username (username): " username
-read -sp "Enter the password (password): " password
+read -p "Enter the role name (role_name) [default dev]: " role_name
+read -p "Enter the user policy name (user_policy_name) [default dev-aws-user]:: " user_policy_name
+read -p "Enter the application policy name (app_policy_name) [ default dev-aws-app]: " app_policy_name
+read -p "Enter the username (username) for inital namespace user: " username
+read -sp "Enter the password (password)for inital namespace password: " password
 echo
-read -sp "Enter the admin password (admin_password): " admin_password
 echo
 
 # Export the environment variables
@@ -34,8 +31,7 @@ export TF_VAR_admin_password="$admin_password"
 
 # Confirm and print the exported variables
 echo "The following environment variables have been set:"
-echo "TF_VAR_vault_address=$TF_VAR_vault_address"
-echo "TF_VAR_vault_admin_token=$TF_VAR_vault_admin_token"
+echo
 echo "TF_VAR_namespace=$TF_VAR_namespace"
 echo "TF_VAR_aws_access_key=$TF_VAR_aws_access_key"
 echo "TF_VAR_aws_secret_key=********"  # Do not print secret keys
@@ -45,7 +41,6 @@ echo "TF_VAR_user_policy_name=$TF_VAR_user_policy_name"
 echo "TF_VAR_app_policy_name=$TF_VAR_app_policy_name"
 echo "TF_VAR_username=$TF_VAR_username"
 echo "TF_VAR_password=********"  # Do not print passwords
-echo "TF_VAR_admin_password=********"  # Do not print passwords
 
 # Optionally, you can run Terraform commands here if desired
  terraform init
