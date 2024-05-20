cd404cdddf306538538bdea8a7e6148f7eb48ecf
Flaxo P <jack.f.pas@gmail.com>
2024-05-20 10:52:14 -0400

    removed namespace block from vault aws secret backend resource

--- modules/aws-secrets/secrets.tf
+++ modules/aws-secrets/secrets.tf
@@ -3,7 +3,6 @@ resource "vault_aws_secret_backend" "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
   region     = var.aws_region
-  namespace  = var.namespace
 }
 
 resource "vault_aws_secret_backend_role" "role" {
